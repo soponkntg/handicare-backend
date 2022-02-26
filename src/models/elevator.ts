847762e18b773577f91634e796b35f672b144f78
latte34844 <73719213+latte34844@users.noreply.github.com>
2022-02-26 23:29:27 +0700

    change lift to elevator

---
+++ src/models/elevator.ts
@@ -0,0 +1,30 @@
+import Sequelize from "sequelize";
+import sequelize from "../database";
+
+const Elevator = sequelize.define("elevator", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  switch: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+  },
+  passable: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+  },
+  located: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  floor: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  remark: Sequelize.STRING,
+});
+
+export default Elevator;
