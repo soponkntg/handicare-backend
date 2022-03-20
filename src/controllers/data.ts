6ff243a567d8303cd0283a4a94adafe7fa83c984
latte34844 <73719213+latte34844@users.noreply.github.com>
2022-03-20 19:12:17 +0700

    locationrestaurant

--- src/controllers/data.ts
+++ src/controllers/data.ts
@@ -1,28 +1,73 @@
+import { Sequelize } from "sequelize";
 import { Request, Response } from "express";
-import { userInfo } from "os";
-import { Location } from "../models";
+import {
+  Door,
+  Elevator,
+  Location,
+  LocationRestaurant,
+  Parking,
+  Ramp,
+  Restaurant,
+  Toilet,
+} from "../models";
+import sequelize from "../database";
 
 const getRecommendLocation = async (req: Request, res: Response) => {
-  console.log('Latitude:', req.query.Latitude)
-  console.log('Longitude:', req.query.Longitude)
-  const locations = await Location.findAll();
-  console.log(locations.every(location => location instanceof Location));
-  console.log("locations", JSON.stringify(locations, null, 2))
-  res.send('locations')
+  console.log("Latitude:", req.query.lat);
+  console.log("Longitude:", req.query.lng);
+  //calculate distance
 
+  //query top5 location
+  const locations = await Location.findAll();
+  console.log(locations.every((location) => location instanceof Location));
+  console.log("locations", JSON.stringify(locations, null, 2));
+  res.send("locations");
 };
 const getRecommendRestaurant = async (req: Request, res: Response) => {
+  const responds: {
+    locationID: string;
+    locationtionName: string;
+    restaurantID: string | null;
+    restaurantName: string | null;
+    placeImage: string;
+    ramp: boolean;
+    toilet: boolean;
+    elevator: boolean;
+    door: boolean;
+    parking: boolean;
+    distance: number;
+  }[] = [];
+  const { lat, lng } = req.query as unknown as { lat: number; lng: number };
+  //calculate distance
 
-}
-;
+  //query top5 locationRestaurant or restaurant
+
+  //query top5 locationRestaurabt
+  const lr_5 = await LocationRestaurant.findAll({
+    attributes: ["locationId", "restaurantId", "count"],
+    limit: 5,
+    order: [Sequelize.literal("count DESC")],
+  });
+  console.log(lr_5);
+
+  const l_5 = await Location.findAll({
+    attributes: ["id", "name", "count", "imageURL"],
+    where: { category: "restaurant" },
+    limit: 5,
+    include: [Ramp, Elevator, Parking, Toilet, Door],
+    order: [Sequelize.literal("count DESC")],
+  });
+  console.log(l_5);
+  res.send(lr_5);
+};
 const getAllLocation = async (req: Request, res: Response) => {
-  const locations = await Location.findAll(
-    {attributes: [
-      'id', 'name','imageURL'
-    ]
+  const locations = await Location.findAll({
+    attributes: ["id", "name", "imageURL"],
+    include: [Ramp, Elevator, Parking, Toilet, Door],
+    order: ["name"],
   });
 
-  const response = JSON.stringify(locations, null, 2)
+  const response = JSON.stringify(locations, null, 2);
   console.log(response);
   res.send(response);
 };
