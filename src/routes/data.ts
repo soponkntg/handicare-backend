0da4f4b3e8924d65080d67a8812afc400895d7ed
latte34844 <73719213+latte34844@users.noreply.github.com>
2022-03-27 15:08:46 +0700

    search endpoints

--- src/routes/data.ts
+++ src/routes/data.ts
@@ -6,6 +6,7 @@ router.get("/recommend/location", dataController.getRecommendLocation);
 router.get("/recommend/restaurant", dataController.getRecommendRestaurant);
 router.get("/locations", dataController.getAllLocation);
 router.post("/location", dataController.postLocation);
+router.post("/search", dataController.postSearch);
 router.post("/location/restaurant", dataController.postLocationRestaurant);
 
 export default router;
